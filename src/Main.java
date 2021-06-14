7e3d1471d0fad36294fd517af84680d0a70efc24
Leonid Beltsov <leonidbeltsov@gmail.com>
2021-06-15 00:16:42 +0300

    Commit 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int incoming_transfer = 500_000_000;
+        int total = balance + incoming_transfer;
+        System.out.println(total);
+    }
+}
